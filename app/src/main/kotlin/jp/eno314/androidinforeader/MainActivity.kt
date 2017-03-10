fdd9787ecdfe8adae5203eb57903b4cec96ebdde
Hiroto Kitamura <kirikonknow@gmail.com>
2017-03-11 08:49:03 +0900

    databindingの設定

--- app/src/main/kotlin/jp/eno314/androidinforeader/MainActivity.kt
+++ app/src/main/kotlin/jp/eno314/androidinforeader/MainActivity.kt
@@ -2,10 +2,17 @@ package jp.eno314.androidinforeader
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.databinding.DataBindingUtil
+
+import jp.eno314.androidinforeader.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding: ActivityMainBinding
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.mainText.text = "hello world"
     }
 }
