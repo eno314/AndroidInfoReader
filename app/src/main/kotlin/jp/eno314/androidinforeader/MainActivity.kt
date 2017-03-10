f9448eb150953aeeb673d1b36221089c8302849d
Hiroto Kitamura <kirikonknow@gmail.com>
2017-03-11 08:56:17 +0900

    kotlinディレクトリをsourceSetsに追加

--- app/src/main/kotlin/jp/eno314/androidinforeader/MainActivity.kt
+++ app/src/main/kotlin/jp/eno314/androidinforeader/MainActivity.kt
@@ -1,18 +1,17 @@
 package jp.eno314.androidinforeader
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.databinding.DataBindingUtil
-
 import jp.eno314.androidinforeader.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        binding.mainText.text = "hello world"
+        val binding: ActivityMainBinding = DataBindingUtil
+                .setContentView(this, R.layout.activity_main)
+        binding.mainText.text = "hello world !!!"
     }
 }
